56bcc7262023468230c2a20cca949ebb67837de6
Maybeiley <2784519@gmail.com>
2024-10-21 17:10:38 +0900

    Fix: fix structs of article and comment

--- structs.js
+++ structs.js
@@ -15,20 +15,20 @@ export const PatchProduct = s.partial(CreateProduct);
 // 게시글과 댓글에 대한 조건은 임의로 설정함
 
 export const CreateArticle = s.object({
-  title: s.size(s.string(), 5, 20),
-  content: s.size(s.string(), 10),
+  title: s.string(),
+  content: s.string(),
 });
 
 export const PatchArticle = s.partial(CreateArticle);
 
 export const CreateProductComment = s.object({
-  content: s.size(s.string(), 5, 200),
+  content: s.string(),
 });
 
 export const PatchProductComment = s.partial(CreateProductComment);
 
 export const CreateArticleComment = s.object({
-  content: s.size(s.string(), 5, 200),
+  content: s.string(),
 });
 
 export const PatchArticleComment = s.partial(CreateArticleComment);
